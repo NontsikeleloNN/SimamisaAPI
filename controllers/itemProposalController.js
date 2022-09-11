fe8db5989b4cf3bfb3a1f8f7d7b1c93c97b38042
OnalennaKetshabile <220150475@student.uj.ac.za>
2022-09-11 21:56:30 +0200

    item proposal get by orphanage

--- controllers/itemProposalController.js
+++ controllers/itemProposalController.js
@@ -38,11 +38,29 @@ async function calcAmountReceived(id){
 const getAllProposals = async(req, res) =>{
     let proposals = await ItemProposal.findAll({});
     if(!proposals) {
-        return res.status(404).send('there are no proposals')
+        return res.status(404).json('there are no proposals')
     }
     res.status(200).json(proposals)
 }
 
+const getOrphanageProposals = async(req, res) =>{
+    try {
+        const id = req.query.id // orphID
+    var props  = []
+    var items = await ItemNeed.findAll({where : {orphanageID : id}})
+
+    for (const i of items) {
+        var temp = await ItemProposal.findAll({where: {itemNeedID : i.ID}})
+        props.push(...temp)
+    }
+
+    res.status(200).json(props)
+    } catch (error) {
+        res.status(500).json({
+            errorMessage: error.message
+       })
+    }
+}
 
 const getUnfulfilled = async(req,res) =>{
     let proposals = await ItemProposal.findAll({where:{isFulfilled : false}});
@@ -70,15 +88,22 @@ const getUserProposals = async(req,res) =>{
 }
 
 const getProposal = async(req,res) =>{
-    let id = req.params.proposalID
-    let proposals = await ItemProposal.findAll({where: {ID:id}})
+   try {
+    let id = req.query.id
+    let proposals = await ItemProposal.findOne({where: {ID:id}})
     console.log(id)
         if(!proposals) return res.status(404).send('there are no proposals for this item')
 
     res.status(200).json(proposals)
+   } catch (error) {
+    res.status(500).json({
+        errorMessage: error.message
+   })
+   }
 }
 
 const createProposal = async(req,res) =>{
+try {
     let newProposal = {
         PickUpPlace : req.body.PickUpPlace,
         PickUpTime : req.body.PickUpTime,
@@ -98,6 +123,11 @@ const createProposal = async(req,res) =>{
        
         
     res.status(200).json(savedProposal)
+} catch (error) {
+    res.status(500).json({
+        errorMessage: error.message
+   })
+}
 
 }
 
@@ -107,11 +137,11 @@ const updateProposal = async(req,res) => {
     // I think I should just receive the changes that I want to and update them accordingly
 
     const proposal = await ItemProposal.update(req.body,{where: {ID : id}})
-        if(!proposal) return res.status(400).send('could not update item: '+ id);
+        if(!proposal) return res.status(400).json('could not update item: '+ id);
 
         res.status(200).json(proposal)
    } catch (error) {
-    res.status(500).send({
+    res.status(500).json({
         errorMessage: error.message
    })
    }
@@ -188,5 +218,6 @@ module.exports = {
     getUnfulfilled,
     getUserProposals,
     calcItemsReceived,
-    donate // add to router and test 
+    donate, // add to router and test 
+    getOrphanageProposals
 }
--- routers/itemProposalRouter.js
+++ routers/itemProposalRouter.js
@@ -2,11 +2,11 @@ const itemProposalController = require('../controllers/itemProposalController');
 const router = require('express').Router();
 
 router.get('/',itemProposalController.getAllProposals)
-router.get('/:id',itemProposalController.getProposal)
+router.get('/',itemProposalController.getProposal)
 router.get('/item/:id',itemProposalController.getItemProposals)
 router.get('/user/:id',itemProposalController.getUserProposals)
 router.get('/unfulfilled',itemProposalController.getUnfulfilled)
-
+router.get('/orphanage',itemProposalController.getOrphanageProposals)
 router.post('/item' ,itemProposalController.createProposal)
 router.post('/donation',itemProposalController.donate) //tested : input in the body itemid = id, comment + ProposalComment, userID = registeredUserID, donation amount = amount
 router.put('/',itemProposalController.updateProposal)
