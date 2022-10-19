de9f4fbdd9ba352add54e70dd338824d1d8ef191
OmphileMonchwe <omphile05monchwe@gmail.com>
2022-10-19 07:22:28 +0200

    partnering fix

--- controllers/partneringController.js
+++ controllers/partneringController.js
@@ -434,8 +434,8 @@ const getMyOffers = async (req, res) => {
                 where: {
                     ReceivingPartner: id,
                     offerID: element.ID, 
-                    // isRejected : true, 
-                    // isAccepted : true
+                    isRejected : true, 
+                    isAccepted : true
                 }
             })
             if(temp != null){
@@ -445,7 +445,7 @@ const getMyOffers = async (req, res) => {
             }
    
         }
-
+     offerItemArray = offerItemArray.filter((item)=>{return !(item.isAccepted&&!item.isRejected) || !(item.isRejected&& !item.isAccepted)})
       for (let a of offerItemArray ) {
             if(a != null ) {
              var temp = {ID: '', ReceivingPartner: '', AmountTaken: '', Title: '', Description: '', Quantity: '' , Sender: ''}
